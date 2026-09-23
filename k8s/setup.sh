b9f77d28e99de90d910ab60d43dc38f9751c9e07
Vu <tranngocvu4220@gmail.com>
2026-09-24 02:12:42 +0700

    add kafka ui manifest

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -3,3 +3,5 @@ kubectl create namespace argocd
 kubectl create namespace order-inventory
 
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl create -f 'https://strimzi.io/install/latest?namespace=order-inventory' -n order-inventory

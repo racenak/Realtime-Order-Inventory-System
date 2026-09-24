ddddab94f547b911a292cba09d93ead590e6b6ee
Vu <tranngocvu4220@gmail.com>
2026-09-24 21:03:46 +0700

    fix can not connect HPA [skip ci]

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -1,8 +1,10 @@
 minikube start --driver=docker --cpus=4
+minikube addons enable metrics-server
 kubectl create namespace argocd 
 kubectl create namespace order-inventory
 
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl create -f 'https://strimzi.io/install/latest?namespace=order-inventory' -n order-inventory
 
 kubectl -n argocd get secret argocd-initial-admin-secret \
   -o jsonpath="{.data.password}" | base64 -d

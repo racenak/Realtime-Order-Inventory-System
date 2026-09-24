acc554bb8981a61632ccc0a6546b0faf515d3104
Vu <tranngocvu4220@gmail.com>
2026-09-24 15:56:49 +0700

    fix can not connect HPA

--- k8s/setup.sh
+++ k8s/setup.sh
@@ -4,4 +4,6 @@ kubectl create namespace order-inventory
 
 kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-kubectl create -f 'https://strimzi.io/install/latest?namespace=order-inventory' -n order-inventory
+kubectl -n argocd get secret argocd-initial-admin-secret \
+  -o jsonpath="{.data.password}" | base64 -d
+echo
